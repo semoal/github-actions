18e977c6051c55e314442eb7d42539ecec80d815
Sergio Moreno <sergiomorenoalbert@gmail.com>
2020-11-13 16:47:49 +0100

    Update entrypoint.sh

--- publish/entrypoint.sh
+++ publish/entrypoint.sh
@@ -15,4 +15,7 @@ sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_regis
 # login so we can publish packages
 sh -c "npm-auth-to-token -u test -p test -e test@test.com -r $local_registry"
 # Run nmp command
-sh -c "npm publish --registry $local_registry $1"
+sh -c "./node scripts/verdaccio-release.js"
+
+sh -c "./node scripts/verdaccio-integration.js"
+
